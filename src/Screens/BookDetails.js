e880ab77966f9aa2145da903c9b84d635741283e
RodrigoQuiroz09 <a01026546@itesm.mx>
2023-04-25 22:03:05 -0600

    Full pantalla de Detalles del Libro

--- src/Screens/BookDetails.js
+++ src/Screens/BookDetails.js
@@ -1,12 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import store from "../Store/store";
 import { setCart } from "../Store/slices/cartSlice";
+import Rating from "@mui/material/Rating";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Divider from "@mui/material/Divider";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { Link } from "react-router-dom";
+
+import HomeIcon from "@mui/icons-material/Home";
 import "./BookDetails.css";
 import swal from "sweetalert";
 
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: "#eaeaea",
+  backgroundColor: "#151111",
+  "&:hover": {
+    backgroundColor: "#476259",
+  },
+}));
+
+const ColorDivider = styled(Divider)(({ theme }) => ({
+  borderColor: "#d8d8d8",
+}));
+
 const BookDetails = () => {
+  const matches = useMediaQuery("(max-width:1250px)");
   const { id } = useParams();
   const book =
     useSelector((state) => state.books.find((book) => book.SKU === id)) ??
@@ -18,7 +40,9 @@ const BookDetails = () => {
     window.localStorage.setItem("book", JSON.stringify(book));
   }, [id, book]);
 
-  console.log(book);
+  const truncate = (str, n) => {
+    return str.length > n ? str.slice(0, n - 1) + " [...]" : str;
+  };
 
   const comprarLibro = async (SKU) => {
     if (localCartItems === null) {
@@ -34,25 +58,88 @@ const BookDetails = () => {
   };
   return (
     <div className="books-details-wrap">
-      <div className="box-books">
-        <div className="inner-box-books">
-          <div className="left-container">
-            <img src={book.img} alt={book.titulo} />
-          </div>
-          <div className="middle-container">
-            <div>
-              <p>Titulo: {book.titulo}</p>
-              <p>Autor: {book.autor}</p>
-              <p>Sinopsis:{book.sin}</p>
-              <p>Precio: {`$${book.precio}`}</p>
+      <div className="card-background">
+        <div className="box-books">
+          <div className="inner-box-books">
+            <div className="left-container">
+              <img src={book.img} alt={book.titulo} />
             </div>
-            <div>
-              <button onClick={() => comprarLibro(book.SKU)} disabled={disable}>
-                {disable ? "En carrito" : "Comprar"}
-              </button>
+            <hr></hr>
+            <div className="middle-container">
+              <div>
+                <p className="author"> {book.autor}</p>
+                <p className="title"> {book.titulo}</p>
+                <div className="rating">
+                  <Rating
+                    name="half-rating"
+                    defaultValue={book.rating}
+                    precision={0.1}
+                    readOnly
+                    size="small"
+                  />
+                  <p className="no_rating">{book.count_rating} Ratings</p>
+                </div>
+
+                <p className="sinopsis">{truncate(book.sin, 600)}</p>
+              </div>
+              <div className="buttons-area">
+                <ColorButton
+                  onClick={() => comprarLibro(book.SKU)}
+                  disabled={disable}
+                  variant="contained"
+                  size={matches ? "small" : "medium"}
+                  startIcon={<AddShoppingCartIcon />}
+                >
+                  {disable ? "En carrito" : `Comprar $${book.precio}`}
+                </ColorButton>
+                <Link to="/">
+                  <ColorButton
+                    onClick={() => console.log("a")}
+                    variant="contained"
+                    size={matches ? "small" : "medium"}
+                    startIcon={<HomeIcon />}
+                  >
+                    Return Home
+                  </ColorButton>
+                </Link>
+              </div>
+            </div>
+            <hr></hr>
+            <div className="vertical-divider">
+              <ColorDivider
+                orientation="vertical"
+                variant="middle"
+                light
+                absolute
+              />
+            </div>
+
+            <div className="right-container">
+              <div className="title">BOOK DETAILS</div>
+              <div className="texto-libro-container">
+                <div className="book-text-pair">
+                  <div className="negritas">ISBN-10: </div>
+                  <div className="texto">{book.SKU}</div>
+                </div>
+                <div className="book-text-pair">
+                  <div className="negritas">Genre: </div>
+                  <div className="texto">{book.genre}</div>
+                </div>
+                <div className="book-text-pair">
+                  <div className="negritas">Year: </div>
+                  <div className="texto">{book.year}</div>
+                </div>
+                <div className="book-text-pair">
+                  <div className="negritas">Num pages: </div>
+                  <div className="texto">{book.num_pages}</div>
+                </div>
+                <div className="book-text-pair">
+                  <div className="negritas">Rating: </div>
+                  <div className="texto">{book.rating}</div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="right-container"></div>
         </div>
       </div>
     </div>
